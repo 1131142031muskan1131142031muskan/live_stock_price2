d2316550719e48549602aca16b6ddf7968143182
1131142031muskan1131142031muskan <80273711+1131142031muskan1131142031muskan@users.noreply.github.com>
2025-01-17 00:11:00 +0530

    Update live_stock_price2.py

--- live_stock_price2.py
+++ live_stock_price2.py
@@ -9,7 +9,7 @@ ticker = st.sidebar.text_input("Enter Stock Ticker Symbol (e.g., INFY)", "INFY")
 exchange = st.sidebar.text_input("Enter Exchange (e.g., NSE)", "NSE")
 
 # API URL (use the deployed URL of your Flask API here)
-api_url = f'https://flask-api-2-mgs8.onrender.com/get_stock_price?ticker=INFY&exchange=NSE/get_stock_price?ticker={ticker}&exchange={exchange}'
+api_url = f'https://flask-api-2-mgs8.onrender.com/get_stock_price?ticker={ticker}&exchange={exchange}'
 
 # Fetch Stock Price
 try:
@@ -18,7 +18,7 @@ try:
         data = response.json()
         if "price" in data:
             st.subheader("Current Stock Price")
-            st.write(f"The stock price of {data['ticker']} on {data['exchange']} is: ₹{data['price']}")
+            st.write(f"The stock price of {ticker} on {exchange} is: ₹{data['price']}")
         else:
             st.error(data.get("error", "Unknown error occurred"))
     else:
